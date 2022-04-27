2e10bdcf0eec472a53c5f3837989af92e0a98cc6
Gunter <mashyn088@gmail.com>
2022-04-27 11:24:54 +0300

    Js comments

--- js/script.js
+++ js/script.js
@@ -6,16 +6,21 @@ function activeLink(){
 }
 list.forEach((item) =>
 item.addEventListener('click',activeLink));
+//* =====Selector Menu Start=====
 $('ul.selector__menu').on('click', 'li:not(.selector__item_active)', function() {
 	$(this)
 		.addClass('selector__item_active').siblings().removeClass('selector__item_active')
 		.closest('body').find('section.block__content').removeClass('block__content_active').eq($(this).index()).addClass('block__content_active');
 });
+//* =====Selector Menu Start=====
+//! =====Click on Carousel Cards Start=====
 $('div.owl-carousel').on('click', 'div:not(.carousel__card_active)', function() {
 	$(this)
 		.addClass('carousel__card_active').siblings().removeClass('carousel__card_active')
 		// .closest('body').find('div.carousel__card').removeClass('carousel__card_active').eq($(this).index()).addClass('carousel__card');
 });
+//! =====Click on Carousel Cards End=====
+//* =====Responsive Cards Start=====
 VanillaTilt.init(document.querySelectorAll(".dancer__item"), {
     max: 10,
     speed: 1000,
@@ -24,6 +29,8 @@ VanillaTilt.init(document.querySelectorAll(".dancer__item_last"), {
     max: 5,
     speed: 1000,
 });
+//* =====Responsive Cards End=====
+//* =====Carousel Start=====
 const prevIcon = '<img src="./icons/arrows/arrow_left.svg" alt="prev">';
 const nextIcon = '<img src="./icons/arrows/arrow_right.svg" alt="next">';
 $('.owl-carousel').owlCarousel({
@@ -50,6 +57,7 @@ $('.owl-carousel').owlCarousel({
         }
     }
 });
+//* =====Carousel End=====
 // $('ul.footer__menu').on('click', 'li:not(.footer__item_active)', function() {
 // 	$(this)
 // 		.addClass('footer__item_active').siblings().removeClass('footer__item_active')
